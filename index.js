c7ddd5d6c25f22ca8eff782276c6656fc5696c41
sarahbroton <sarah.broton@gmail.com>
2023-07-16 17:01:11 -0500

    updatespackage.json

--- index.js
+++ index.js
@@ -1,5 +1,69 @@
-const express = require('express'); 
-const http = require("http"); 
+// prompt about what svg looks likes
+var answers = []; 
+const userResponses = (''); 
+const userInfo = (''); 
+// const inquirerResponse = (''); 
+// const data = (''); 
 
-const PORT = 3000; 
+// = [
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+// const generateMarkdown = require('./Utils/generateMarkdown'); 
 
+// ];
+
+// TODO: Create an array of questions for user input
+questions = [
+    {
+        type: 'input',
+        message: 'What color do you want your shape?',
+        choices: [], 
+        name: 'color',
+    },
+
+    {
+        type: 'input',
+        message: 'What type shape do you want?',
+        choices: ['circle', 'triangle', 'square'], 
+        name: 'shape'
+    },
+    
+    {
+        type: 'input',
+        message: 'What three characters would you like?',
+        name: 'characters'
+    },
+    
+    {
+        type: 'list',
+        message: 'What text color woudl you like?',
+        choices: [],
+        name: 'text', 
+    
+    },
+    
+]
+
+// if/else statements for choices above ADD HERE
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+fs.writeFileSync(fileName, data);
+}
+
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((inquirerResponse) => {
+        console.log(inquirerResponse); 
+        console.log("making readme"); 
+        writeToFile(`${inquirerResponse.fileName}.md`, generateMarkdown(inquirerResponse))
+    })
+    .catch((err) => {
+        console.log(err); 
+    });
+}
+
+
+init();
